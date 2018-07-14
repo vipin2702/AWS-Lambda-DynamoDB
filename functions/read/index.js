9808419bf7aacda26bd4e953ab32bb24d117b4aa
vipin2702 <vipinmittal2702@gmail.com>
2018-07-14 10:46:35 +0530

    Reads data from dynamodb table

---
+++ functions/read/index.js
@@ -0,0 +1,22 @@
+console.log('starting function');
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
+
+exports.handle = function(e, ctx, callback) {
+	
+		let scanningParameters = {
+			TableName: 'guestbook',
+			Limit:100
+		};
+		
+		docClient.scan(scanningParameters, function(err, data){
+			if(err){
+				callback(err, null);
+			}else{
+				callback(null, data);
+			}
+		});	
+		
+		
+}
